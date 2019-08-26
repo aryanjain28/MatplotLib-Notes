f8210d74dc41afda5b039425cd29127387303f3f
Aryan Jain <aryanjn807@gmail.com>
2019-08-26 19:02:25 +0530

    Create 1. Basics.py

---
+++ 1. Basics.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4])     #if we give single list in plot function it takes it as Y-values and generates
+                        # corresponding X-values with first value of x being 0.
+                        # Therefore we get a graph with (0,1)(1,2)(2,3)(3,4) as co-ordinates.
+plt.show()
