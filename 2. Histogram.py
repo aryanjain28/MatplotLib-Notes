6c93ab5264205d015ba1e7f386b16d04a67e3ca1
Aryan Jain <aryanjn807@gmail.com>
2019-08-27 19:10:13 +0530

    Update 2. Histogram.py

--- 2. Histogram.py
+++ 2. Histogram.py
@@ -25,31 +25,3 @@ plt.show()
 # )
 #
 # plt.show()
-
-
-
-
-fig = plt.figure(figsize=(9,3))
-# myAxes = fig.add_subplot(111)
-# 
-# myAxes.plot([1,2,3,4,5,6,7,8,9],[1,4,9,16,25,36,49,64,81])
-# 
-# myAxes.tick_params(
-# 
-#     axis='both',        #x, y, both
-#     which = 'both',      #major, minor, both
-#     direction = 'inout',     #in, out, inout
-#     length = 5,
-#     width = 2,
-#     color = 'r',
-#     pad = 5.0,
-#     labelsize = 8,
-#     labelcolor= 'b',
-#     bottom = False,
-#     top = True,
-#     labelleft = True,
-#     labelright = True,
-#     grid_color = 'k'
-# )
-# 
-# plt.show() 
